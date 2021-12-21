42298bab7bbe3ae9d5d11853b10ef47209c3a038
Ozan Ercan <ozanercann@gmail.com>
2021-12-21 10:02:46 +0300

    react-redux-form revisited baska sayfalara gidince form verileri korunuyor submit edince form resetleniyor

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import OzanContact from './OzanFormComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router';
 import {connect} from 'react-redux'
 import {addComment, fetchDishes} from '../redux/ActionCreators'
+import {actions} from 'react-redux-form'
 
 
 
@@ -26,7 +27,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
+
 });
 
 
@@ -70,7 +73,7 @@ class Main extends Component {
           <Route path='/home' component={HomePage}/>
           <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>} />
           <Route path='/menu/:dishId' component={DishWithId}/>
-          <Route exact path='/contactus' component={Contact}/>
+          <Route exact path='/contactus' component={ () => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/> }/>
           <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}/>} />
           <Route exact path='/ozan' component={OzanContact}/>
           <Redirect to="/home" />
